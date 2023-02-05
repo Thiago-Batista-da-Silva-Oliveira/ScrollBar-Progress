8e545cd2ea1ebb6d52650d3f448d77a36e600b94
Thiago <gmoliveira69@gmail.com>
2023-02-04 23:29:11 -0300

    text styles

--- src/screens/Post/index.tsx
+++ src/screens/Post/index.tsx
@@ -1,12 +1,48 @@
-import { Text, View } from "react-native"
-import { styles } from "./styles"
+import { ScrollView, Text, View } from "react-native";
+import { styles } from "./styles";
 
 export const Post = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.title}>
-                Lorem ipsum
-            </Text>
+  return (
+    <View style={styles.container}>
+      <ScrollView
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={styles.content}
+      >
+        <Text style={styles.title}>Lorem ipsum</Text>
+        <View>
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet illum
+            alias explicabo sint. Sequi soluta veniam magnam ab odit! Assumenda
+            animi officiis eum dicta dignissimos eveniet consequuntur numquam
+            fugit veritatis.
+          </Text>
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet illum
+            alias explicabo sint. Sequi soluta veniam magnam ab odit! Assumenda
+            animi officiis eum dicta dignissimos eveniet consequuntur numquam
+            fugit veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing
+            elit. Amet illum alias explicabo sint. Sequi soluta veniam magnam ab
+            odit! Assumenda animi officiis eum dicta dignissimos eveniet
+            consequuntur numquam fugit veritatis. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Amet illum alias explicabo sint. Sequi
+            soluta veniam magnam ab odit! Assumenda animi officiis eum dicta
+            dignissimos eveniet consequuntur numquam fugit veritatis. Lorem
+            ipsum dolor sit amet, consectetur adipisicing elit. Amet illum alias
+            explicabo sint. Sequi soluta veniam magnam ab odit! Assumenda animi
+            officiis eum dicta dignissimos eveniet consequuntur numquam fugit
+            veritatis. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Amet illum alias explicabo sint. Sequi soluta veniam magnam ab odit!
+            Assumenda animi officiis eum dicta dignissimos eveniet consequuntur
+            numquam fugit veritatis.
+          </Text>
+          <Text style={styles.text}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet illum
+            alias explicabo sint. Sequi soluta veniam magnam ab odit! Assumenda
+            animi officiis eum dicta dignissimos eveniet consequuntur numquam
+            fugit veritatis.
+          </Text>
         </View>
-    )
-}
+      </ScrollView>
+    </View>
+  );
+};

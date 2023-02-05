f0f4d558a9443dc63d7d08a17bdbc731cc50005e
Thiago <gmoliveira69@gmail.com>
2023-02-04 23:23:06 -0300

    setting up

---
+++ src/screens/Post/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native"
+import { styles } from "./styles"
+
+export const Post = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>
+                Lorem ipsum
+            </Text>
+        </View>
+    )
+}
---
+++ src/screens/Post/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#000"
+    },
+    title: {
+        fontSize: 32,
+        color: '#FFF'
+    }
+})

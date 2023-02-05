0fce331995c1676b1d3534a4f5138f6a9248efea
Thiago <gmoliveira69@gmail.com>
2023-02-05 00:18:59 -0300

    arrow up

--- src/components/Progressbar/index.tsx
+++ src/components/Progressbar/index.tsx
@@ -1,4 +1,6 @@
-import { Text, View } from "react-native";
+import { useEffect } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { Feather } from "@expo/vector-icons";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -11,17 +13,32 @@ type Props = {
 
 export const ProgressBar = ({ value }: Props) => {
   const widthContainer = useSharedValue(200);
+  const endReached = value >= 95;
   const animatedStyle = useAnimatedStyle(() => {
     return {
       width: widthContainer.value,
     };
   });
+
+  useEffect(() => {
+    widthContainer.value = endReached ? 56 : 200
+  }, [value])
   return (
     <Animated.View style={[styles.container, animatedStyle]}>
+   {
+    endReached ? (
+       <TouchableOpacity>
+          <Feather name="arrow-up" size={24} color="#C4C4CC" />
+       </TouchableOpacity>
+    ) : (
+     <>
       <Text style={styles.value}>{value.toFixed(0)}%</Text>
-      <View style={styles.tracker}>
-        <View style={[styles.progress, { width: `${value}%` }]} />
-      </View>
+       <View style={styles.tracker}>
+         <View style={[styles.progress, { width: `${value}%` }]} />
+       </View>
+       </>
+    )
+   }
     </Animated.View>
   );
 };
